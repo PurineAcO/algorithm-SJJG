94578e92961e1c972d7d70076b544836cc1639c3
purine <3223145991@qq.com>
2026-03-17 17:30:22 +0800

    rotate.c 优化&debug

--- W1/rotate.c
+++ W1/rotate.c
@@ -18,7 +18,7 @@ int main(){
 
     int cnt1=0;int cnt2=0;int cnt3=0;
     for(int i=1;i<=dim;i++){
-        for(int j=1;j<=dim-i+1;j++){
+        for(int j=1;j<=dim;j++){
             if(matrix[i][j]==matrix[j][dim-i+1]){cnt1++;}
             if(matrix[i][j]==matrix[dim-j+1][i]){cnt2++;}
             if(matrix[i][j]==matrix[dim-i+1][dim-j+1]){cnt3++;}
